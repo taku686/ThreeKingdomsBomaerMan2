913aa532a302f41075c5cea88dea9af362e9473f
TakunoshinOshita <z.takunoshin.oshita@gree.net>
2022-11-16 01:12:32 +0900

    ノーマルボム生成中。

--- Assets/Scripts/Bomb/BombDataBase.cs
+++ Assets/Scripts/Bomb/BombDataBase.cs
@@ -6,7 +6,6 @@ namespace Bomb
     public abstract class BombDataBase : MonoBehaviour, IDisposable
     {
         [SerializeField] protected ParticleSystem explosionEffect;
-        [SerializeField] protected GameObject bombObj;
         protected int damageAmount;
         protected int fireRange;
         protected int playerId;
