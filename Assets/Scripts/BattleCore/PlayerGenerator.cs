50f7d39e3fd7f3ec518cde38f99b2a5d6b6003a0
DESKTOP <takunoshin123456789@gmail.com>
2023-04-04 18:21:51 +0900

    Playerの大きさを調整。

--- Assets/Scripts/BattleCore/PlayerGenerator.cs
+++ Assets/Scripts/BattleCore/PlayerGenerator.cs
@@ -11,13 +11,15 @@ namespace Manager.BattleManager
         [SerializeField] private List<Transform> startPointList;
         [SerializeField] private Transform playerParent;
         private GameObject _playerObj;
-        private static readonly int PlayerNotification = 1;
+        private const int PlayerNotification = 1;
+        private const float PlayerSize = 0.8f;
 
         public void GenerateCharacter(int playerIndex, CharacterData characterData)
         {
             var spawnPoint = GetSpawnPoint(playerIndex);
             _playerObj = PhotonNetwork.Instantiate(GameCommonData.CharacterPrefabPath + characterData.CharaObj,
                 spawnPoint.position, spawnPoint.rotation);
+            _playerObj.transform.localScale *= PlayerSize;
             _playerObj.transform.SetParent(playerParent);
             PlayerGenerateNotification();
         }
